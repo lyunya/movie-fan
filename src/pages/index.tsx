63c5e2cdbc83dfe0f177192dedfa57e399af9a78
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-05 15:25:02 -0800

    added types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import { type GetServerSideProps, type NextPage } from "next";
 import Layout from "@layout/default";
 import MovieCard from "@/components/MovieCard/MovieCard";
 import { getNowPlayingMovies } from "../hooks/getNowPlayingMovies";
-import { HomePageProps } from "@/types/main";
+import type { HomePageProps } from "@/types/main";
+import type { MovieCardProps } from "@/components/MovieCard/types";
 
 
 
@@ -15,7 +16,7 @@ const { results:movies } = data
       <main className="flex min-h-screen flex-col items-center pt-4 bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h2 className="text-white text-4xl">See what&apos;s now Playing</h2>
         <div className="max-w-[90%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-4">
-        {movies.slice(0,12).map((movie, idx) => {
+        {movies.slice(0,12).map((movie:MovieCardProps, idx) => {
         return <MovieCard key={idx} {...movie} />
         })}
         </div>
