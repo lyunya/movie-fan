7c95bdbda448c13baa27fe8071059d1634f77886
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-05 15:02:25 -0800

    trying to get react query working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,26 @@
-import { type NextPage } from "next";
+import { type GetStaticProps, type NextPage } from "next";
 import Layout from "@layout/default";
 import MovieCard from "@/components/MovieCard/MovieCard";
 import { useQuery } from "@tanstack/react-query";
 import { getNowPlayingMovies } from "../hooks/getNowPlayingMovies";
 
+export const getStaticProps: GetStaticProps = async () => { 
+  const movies = await getNowPlayingMovies()
+  return { props: { movies } }
+}
 
-const Home: NextPage = (props) => {
+const Home: NextPage = ({ movies }) => {
 
   const { data, error, isLoading } = useQuery({
     queryKey: ['NowPlaying'],
     queryFn: getNowPlayingMovies,
-    initalData: props
+    initalData: movies.results
   })
 
 if (error) return <div>Request Failed</div>;
 if (isLoading) return <div>Loading...</div>;
 
-const { results:movies } = data
+const { results:moviesNow } = data
 
   return (
     <Layout >
@@ -24,7 +28,7 @@ const { results:movies } = data
         <h2 className="text-white text-4xl">See what&apos;s now Playing</h2>
         <div className="max-w-[90%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-4">
           {console.log(movies)}
-        {movies.slice(0,12).map((movie, idx) => {
+        {moviesNow.slice(0,12).map((movie, idx) => {
         return <MovieCard key={idx} {...movie} />
         })}
         </div>
@@ -33,9 +37,6 @@ const { results:movies } = data
   );
 };
 
-export const getStaticProps = async () => { 
-  const movies = await getNowPlayingMovies()
-  return { props: { movies } }
-}
+
 
 export default Home
