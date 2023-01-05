edcb1ee1786565c5b54caac0ad2601a29ad0215f
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-05 15:14:33 -0800

    not using react query now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,20 @@
-import { type GetStaticProps, type NextPage } from "next";
+import { type GetServerSideProps, type NextPage } from "next";
 import Layout from "@layout/default";
 import MovieCard from "@/components/MovieCard/MovieCard";
-import { useQuery } from "@tanstack/react-query";
 import { getNowPlayingMovies } from "../hooks/getNowPlayingMovies";
 
-export const getStaticProps: GetStaticProps = async () => { 
-  const movies = await getNowPlayingMovies()
-  return { props: { movies } }
-}
 
-const Home: NextPage = ({ movies }) => {
 
-  const { data, error, isLoading } = useQuery({
-    queryKey: ['NowPlaying'],
-    queryFn: getNowPlayingMovies,
-    initalData: movies.results
-  })
+const Home: NextPage = ({ data }) => {
 
-if (error) return <div>Request Failed</div>;
-if (isLoading) return <div>Loading...</div>;
-
-const { results:moviesNow } = data
+const { results:movies } = data
 
   return (
     <Layout >
       <main className="flex min-h-screen flex-col items-center pt-4 bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h2 className="text-white text-4xl">See what&apos;s now Playing</h2>
         <div className="max-w-[90%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-4">
-          {console.log(movies)}
-        {moviesNow.slice(0,12).map((movie, idx) => {
+        {movies.slice(0,12).map((movie, idx) => {
         return <MovieCard key={idx} {...movie} />
         })}
         </div>
@@ -37,6 +23,14 @@ const { results:moviesNow } = data
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async () => { 
+try {
+	  const data = await getNowPlayingMovies()
+	  return { props: { data } }
+} catch (error) {
+	return { props: { error } }
+}
+}
 
 
 export default Home
