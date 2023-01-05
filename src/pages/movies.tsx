5a935236c888a1b60a269b4fc72e155123b22d9c
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-05 11:05:54 -0800

    need to change movies page

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -1,12 +1,13 @@
 import { type NextPage } from "next";
 import Layout from "@layout/default";
-import nowPlayingMovies from '../data/nowPlaying.json'
+// import nowPlayingMovies from '../data/nowPlaying.json'
 
 const movies: NextPage = () => {
-  const currentMovies = nowPlayingMovies;
+  // const currentMovies = nowPlayingMovies;
   return (
     <Layout >
-      <h2> {currentMovies.map(movie => movie.title)}</h2> 
+      {/* <h2> {currentMovies.map(movie => movie.title)}</h2>  */}
+      <h2>Leon</h2>
       </Layout>
   )
 }
