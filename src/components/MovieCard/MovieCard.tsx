870f403504d73bd9f25a0a24bf6308edf1a7d205
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-11 15:12:38 -0800

    resp hover movie cards

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -8,7 +8,7 @@ const MovieCard: FC<MovieCardProps> = ({ name, posterImage, emsVersionId }) => {
     <Link href={`/movie/${emsVersionId}`}>
     <div className='flex  min-h-[400px] min-w-[300px] xl:min-h-[480px] xl:min-w-[384px] flex-col relative items-center bg-white border rounded-lg cursor-pointer snap-start mx-4 w-full'>
       <Image className='h-full absolute inset-0 bg-cover bg-center z-0 rounded-lg w-full' src={posterImage?.url || '/placeholderposter.png'} height={480} width={384} alt='movie poster' />
-      <div className='grid place-items-center	 h-full p-4 items-center opacity-0 hover:opacity-90  absolute inset-0 z-10 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 text-white rounded-lg'>
+      <div className='grid place-items-center	 h-full p-4 items-center opacity-0 sm:hover:opacity-90  absolute inset-0 z-10 sm:hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 text-white rounded-lg'>
       <h2 className='mb-4 text-center text-3xl'>{name}</h2>
         {/* <div className='mt-auto text-center'>
         <p className='flex w-fit justify-between mt-auto text-xl'>
