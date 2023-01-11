4e0bad3075196b6d3bb0e728bc492a100151860f
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-11 15:08:38 -0800

    removed hover on mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
         heading: [`var(--font-overPass)`],
         body: [`var(--font-kronaOne)`],
       },
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
+      future: {
+        hoverOnlyWhenSupported: true,
       },
     },
   },
