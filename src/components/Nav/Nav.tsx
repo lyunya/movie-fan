73168aadb2811c0f134cf1319b31a2186ae59a8b
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-11 15:33:29 -0800

    updated nav button

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,14 +2,27 @@ import type { FC } from 'react'
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "../../utils/api";
 import Link from 'next/link';
+import {useRouter} from 'next/router';
+
 
 const Nav: FC = () => {
+  const router = useRouter()
+  const home = router.pathname === '/';
+
+  console.log(router.pathname, 'this pathname')
+
   return (
     <nav className='relative h-18 p-8 bg-black w-full flex items-center justify-between'>
       <Link href='/'>
       <h1 className='text-4xl sm:text-5xl font-heading font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600'>Movie Fan</h1>
       </Link>
-      <AuthShowcase />
+      {home ? <AuthShowcase /> :
+        <Link href='/'>
+          <button className='text-md sm:text-xl bg-blue-500 hover:bg-blue-700 py-2 px-4 bordered font-heading text-white rounded'>
+            Home
+          </button>
+        </Link>}
+      
     </nav>
   )
 }
