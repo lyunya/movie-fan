56b5caf52a07b935794f10ae2cc685641190b5ae
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-11 15:19:50 -0800

    slight update to skel

--- src/components/MovieDetails/Skeleton.tsx
+++ src/components/MovieDetails/Skeleton.tsx
@@ -5,12 +5,12 @@ const MovieSkeleton = () => {
   return (
     <div className="container mx-auto px-4 pb-20 h-full">
       <div className="flex flex-col justify-evenly md:flex-row items-center">
-        <div className='h-fit w-1/2'>
-          <p className='text-3xl xl:text-5xl pb-2 text-white pr-12'>         
+        <div className='h-fit w-full sm:w-1/2'>
+          <p className='text-3xl xl:text-5xl pb-2 text-white sm:pr-12'>         
             <Skeleton className='mb-2' count={5} enableAnimation={true} />
           </p>
         </div>
-        <div className='h-64 w-64'>
+        <div className='h-72 w-72'>
           <Skeleton height='100%' /> 
         </div>
 
