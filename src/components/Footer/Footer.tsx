777e73fb236ca75b0aa6f3b7cf629796720031bd
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-16 16:36:12 -0800

    fixed mobile footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
+import Balancer from "react-wrap-balancer"
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-[#1e1e1e] pb-4 text-white">
-      <p className="text-center text-2xl">
+    <footer className="w-full bg-[#1e1e1e] pb-4 text-white text-center">
+      <Balancer className="text-2xl">
         Made with ❤️ by{' '}
         <a href="https://leonmarbukh.com/" className="underline">
           Leon Marbukh
         </a>
-      </p>
+      </Balancer>
     </footer>
   )
 }
