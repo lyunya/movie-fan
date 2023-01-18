34c7d22e1636df99048ace5393a91215f41843f7
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-17 20:54:26 -0800

    guh

--- src/utils/getSearchMovies.ts
+++ src/utils/getSearchMovies.ts
@@ -3,10 +3,10 @@ import { RAPID_API_HOST, FLIXSTER_API_SEARCH_URL } from "@/data/Contstants"
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.RAPID_API_KEY as string,
-    'X-RapidAPI-Host': RAPID_API_HOST
-	}
-};
+    'X-RapidAPI-Key': 'd8739849e9msh9de0a072a19f9edp1762cejsne12be3c09936',
+    'X-RapidAPI-Host': RAPID_API_HOST,
+  },
+}
 
 export const getSearchMovies = async (query: string) => { 
   const res = await fetch(`${FLIXSTER_API_SEARCH_URL}${query}`, options)
