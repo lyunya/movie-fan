11e507e496a5e2d8054f00055f8ad44a6312ca70
Leon Marbukh <leonmarbukh@gmail.com>
2023-01-19 14:10:09 -0800

    updated mobile styling

--- src/components/CastGrid/CastGrid.tsx
+++ src/components/CastGrid/CastGrid.tsx
@@ -7,7 +7,7 @@ const CastGrid:FC<CastGridProps> = ({ cast }) => {
     <div className="my-12 text-white lg:col-start-1 lg:col-end-5">
       <>
         <h3 className="mb-4 text-xl">Cast</h3>
-        <div className="grid-credit-cards mx-auto grid auto-rows-fr gap-y-4 2xl:grid-cols-4	">
+        <div className="sm:grid-cols-3 md:grid-credit-cards	gap-x-2 mx-auto grid auto-rows-fr grid-cols-2 gap-y-4 2xl:grid-cols-4	">
           {cast.map((castMember) => {
             return <CastCard key={castMember.id} {...castMember} />
           })}
